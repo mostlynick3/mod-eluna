ea420245929426f550d0853edc3c5c79750a83c6
mostlynick3 <190055638+mostlynick3@users.noreply.github.com>
2025-06-28 20:19:19 +0000

    Optimize Eluna include to only expose LuaValue.h

--- cmake/after_game.cmake
+++ cmake/after_game.cmake
@@ -2,14 +2,20 @@ target_include_directories(game
     PRIVATE
         ${CMAKE_SOURCE_DIR}/modules/mod-eluna/src/LuaEngine
 )
-
 target_link_libraries(game
     PRIVATE
         lualib
 )
 
-# Add Eluna headers to game target so core files can include them for Map and WorldObject data methods
+set(ELUNA_SELECTIVE_DIR ${CMAKE_BINARY_DIR}/eluna_selective)
+file(MAKE_DIRECTORY ${ELUNA_SELECTIVE_DIR})
+configure_file(
+    ${MOD_ELUNA_DIR}/src/LuaEngine/LuaValue.h
+    ${ELUNA_SELECTIVE_DIR}/LuaValue.h
+    COPYONLY
+)
+
 target_include_directories(game-interface
-  INTERFACE
-    ${MOD_ELUNA_DIR}/src/LuaEngine
+    INTERFACE
+        ${ELUNA_SELECTIVE_DIR}
 )
